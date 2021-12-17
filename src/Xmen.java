af22d4a2e5792e3ec16ed1d00ca9264edc2c5cce
Julekmeister <julekmeister@gmail.com>
2021-12-17 22:46:46 +0100

    Xmen2

---
+++ src/Xmen.java
@@ -0,0 +1,40 @@
+public class Xmen extends Mutant{
+    protected int powerMod;
+
+    public Xmen() {
+        super();
+        this.powerMod=0;
+    }
+
+    public Xmen(int powerMod) {
+        this.powerMod = powerMod;
+    }
+
+    public Xmen(String imie, String nazwisko, String codename, int powerLevel, int powerMod) {
+        super(imie, nazwisko, codename, powerLevel);
+        this.powerMod = powerMod;
+    }
+
+    @Override
+    protected int calcPower() {
+        return super.calcPower()+powerMod;
+    }
+
+    public int getPowerMod() {
+        return powerMod;
+    }
+
+    public void setPowerMod(int powerMod) {
+        this.powerMod = powerMod;
+    }
+
+    @Override
+    public String toString() {
+        return "Xmen{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", codename='" + codename + '\'' +
+                ", powerLevel=" + powerLevel +
+                '}';
+    }
+}
